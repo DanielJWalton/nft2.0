4e131c8e9fc21d14e1b74e3252a9a214a2116d6d
Tony Gorgioski <tony.gorgioski@gmail.com>
2022-04-02 19:47:09 +1100

    updated

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -60,7 +60,7 @@ function NFTDropPage({ collection }: Props) {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
+                      d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
                     />
                   </svg>
                   <span className="font-poppins text-lg capitalize tracking-wider text-black transition  duration-200 group-hover:text-purple-500 dark:text-white dark:group-hover:text-purple-300">
